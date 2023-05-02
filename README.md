aae162e8339a747343b66dd86c1c4cef57f31daa
NadiaJoy <iminneverwhere@gmail.com>
2023-05-02 18:04:55 +0300

    HM20, fixed issue #3

--- README.md
+++ README.md
@@ -2,10 +2,10 @@ Check-list for getFactorial:
 
 N | Check Name   | Status
 -- | -------------|--------
-1 | Check positive numbers (fact less than int length) | Done
+1 | Check positive numbers (less than int length) | Done
 2 | Check zero  (should return 1)   | Done
-3 | Check negative numbers  (should return my error message)   | Done
-4 | Check fractional number (InputMismatchException) | Done
-5 | Check letters (InputMismatchException) | Done
-6 | Check  numbers > 20 (should return my error message) | Done
+3 | Check negative numbers  (should return StackOverflowError)   | Done
+4 | Check fractional number (NumberFormatException) | Done
+5 | Check letters (NumberFormatException) | Done
+6 | Check  huge numbers  (StackOverflowError) | Done
 
--- src/main/java/MyLovelyMath/GetFactorial.java
+++ src/main/java/MyLovelyMath/GetFactorial.java
@@ -1,6 +1,5 @@
 package MyLovelyMath;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GetFactorial {
@@ -16,30 +15,31 @@ public class GetFactorial {
 
             int number = Integer.parseInt(input);
 
-            if (number < 0 ) {
-                System.out.println("Number should be more 0. Please, try again.");
-           } else {
                 int result = getFactorial.getFactorial(number);
                 System.out.println("Fractal for " + number + " = " + result);
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Something went wrong. I wish I knew what exactly. Please, try again." +
-                        "\n  Error Log: " + e );
+        } catch (StackOverflowError e) {
+            System.out.println("Wow! Your number too huge or negative! Please, try again." +
+                    "\n  Error Log: " + e);
         } catch (NumberFormatException e) {
             System.out.println("Your number is fractional or contains letters or invalid characters. Please, try again." +
-                    "\n  Error Log: " + e );
+                    "\n  Error Log: " + e);
         } finally {
             userNumber.close();
         }
     }
-
+    //      public int getFactorial(int number) {
+//        int fact = 1;
+//
+//        for (int i = 1; i <= number; i++) {
+//            fact = fact * i;
+//        }
+//        return fact;
+//    }
     public int getFactorial(int number) {
-        int fact = 1;
-
-        for (int i = 1; i <= number; i++) {
-            fact = fact * i;
-        }
-        return fact;
+        if (number == 0)
+            return 1;
+        else
+            return (number * getFactorial(number - 1));
     }
 }
 
