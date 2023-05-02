fdc4291be5f6f8c28c458052d0aa5f7bf2a36f21
NadiaJoy <iminneverwhere@gmail.com>
2023-05-02 18:13:48 +0300

    HM20, fixed issue #3

--- src/main/java/MyLovelyMath/GetFactorial.java
+++ src/main/java/MyLovelyMath/GetFactorial.java
@@ -30,7 +30,7 @@ public class GetFactorial {
     //      public int getFactorial(int number) {
 //        int fact = 1;
 //
-//        for (int i = 1; i <= number; i++) {
+//        for (int i = 2; i <= number; i++) {
 //            fact = fact * i;
 //        }
 //        return fact;
