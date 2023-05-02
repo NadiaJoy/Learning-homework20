3bf767a1472675790d0de01a50786fb2e9aef5d5
NadiaJoy <iminneverwhere@gmail.com>
2023-05-02 17:49:27 +0300

    HM20, fixed issue #3

--- src/main/java/MyLovelyMath/GetFactorial.java
+++ src/main/java/MyLovelyMath/GetFactorial.java
@@ -11,18 +11,23 @@ public class GetFactorial {
         Scanner userNumber = new Scanner(System.in);
         System.out.println("Hi! I can count a factorial of your number. " +
                 "\nType your number: ");
+        String input = userNumber.nextLine();
         try {
-            int number = userNumber.nextInt();
-            if (number < 0 || number >= 20) {
-                System.out.println("Number should be more 0 and less 20. Please, try again.");
-            } else {
+
+            int number = Integer.parseInt(input);
+
+            if (number < 0 ) {
+                System.out.println("Number should be more 0. Please, try again.");
+           } else {
                 int result = getFactorial.getFactorial(number);
                 System.out.println("Fractal for " + number + " = " + result);
             }
-        } catch (
-                InputMismatchException e) {
-            System.out.println("Your number is fractional or contains letters or invalid characters. Please, try again." +
+        } catch (InputMismatchException e) {
+            System.out.println("Something went wrong. I wish I knew what exactly. Please, try again." +
                         "\n  Error Log: " + e );
+        } catch (NumberFormatException e) {
+            System.out.println("Your number is fractional or contains letters or invalid characters. Please, try again." +
+                    "\n  Error Log: " + e );
         } finally {
             userNumber.close();
         }
